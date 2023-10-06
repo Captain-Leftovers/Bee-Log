03d48ac722209c13731e5f07047c4f767b1216a8
Dob <lalilulelo.mg@gmail.com>
2023-10-07 09:54:09 +1100

    added colors in tailwind.config

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body
-				className={`${inter.className} flex flex-col bg-red-400 min-h-screen`}
+				className={`${inter.className} flex flex-col min-h-screen bg-background text-primary`}
 			>
 				<Navbar />
 				<main className="grow">{children}</main>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
 	return (
 		<footer className="border-t">
 			<div className="mx-auto py-10">
-				<p className="text-center text-xs text-black">
+				<p className="text-center text-xs">
 					&copy; ${new Date().getFullYear()} Not a real store, Inc
 					All rights reserved.
 				</p>
