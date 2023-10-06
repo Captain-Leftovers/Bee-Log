3536f99aafa430444d77a23b717183ec7f5d9e63
Dob <lalilulelo.mg@gmail.com>
2023-10-07 08:39:19 +1100

    creating the layout

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import Container from './ui/Container'
+
+export default function Navbar() {
+	return (
+		<div className="border-b">
+			<div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+				<Container>Navbar</Container>
+			</div>
+		</div>
+	)
+}
